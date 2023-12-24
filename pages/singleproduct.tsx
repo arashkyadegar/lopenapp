5638ff69eddf13fcb29dc09b49523c702304936d
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-24 20:58:17 +0330

    i finished singleproduct

--- pages/singleproduct.tsx
+++ pages/singleproduct.tsx
@@ -25,7 +25,7 @@ export default function SingleProduct() {
               </a>
             </div>
             <div>
-              <div className="grid grid-cols-1 sm:grid-cols-2 p-10">
+              <div className="grid grid-cols-1 sm:grid-cols-2 p-4">
                 <div className="">
                   {/* single product image */}
                   <div className="text-right relative text-sm  mx-auto  max-w-sm bg-white    border border-[#FBF6EE]           hover:border-[#FFB534] transition duration-200 shadow dark:bg-gray-800 dark:border-gray-700">
@@ -204,7 +204,7 @@ export default function SingleProduct() {
                               required
                             />
                           </div>
-                          <div className=" flex flex-row w-3/4 items-center justify-center bg-slate-300 px-3 py-2  cursor-pointer hover:text-white  hover:bg-[#80BB01] transition-all duration-300">
+                          <div className=" flex flex-row w-1/2 items-center justify-right bg-slate-300 px-3 py-2  cursor-pointer hover:text-white  hover:bg-[#80BB01] transition-all duration-300">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -224,7 +224,7 @@ export default function SingleProduct() {
                           </div>
                         </div>
                       </div>
-                      <a className="text-2xl ">مشخصات</a>
+                      <a className="text-2xl my-2">مشخصات</a>
 
                       <div className="grid grid-cols-5 gap-4">
                         <div className="col-span-2  p-1">
@@ -266,19 +266,85 @@ export default function SingleProduct() {
                 </div>
               </div>
             </div>
+            {/* users comments  */}
+
+            <div className="p-4 mx-4 bg-white    shadow-md shadow-gray-300 my-4 ">
+              <div className="flex flex-row border-b-4 mb-4 px-2 py-1 border-[#EF5621] text-right font-bold">
+                <div className="">
+                  <a> نظرات کاربران</a>
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-2 mb-4 p-4">
+                <div>
+                  <a>متن </a>
+                </div>
+                <textarea
+                  className="w-9/12 border p-2 border-gray-300 outline-none"
+                  name=""
+                  id=""
+                  cols={35}
+                  rows={3}
+                ></textarea>
+                <button className="px-2 bg-slate-300 hover:bg-[#80BB01] w-2/12 h-fit">
+                  ارسال
+                </button>
+              </div>
+              <div className="grid grid-cols-1 gap-2">
+                {/* user single comment */}
+                <div className="flex flex-col border-b even:bg-[#F3F4F8] border-gray-400 p-4 my-2">
+                  <div className="flex flex-row justify-between">
+                    <a className="text-base font-bold ">ارشک یادگار</a>
+                    <a>1392/01/01</a>
+                  </div>
+
+                  <p className="text-sm text-justify mt-4">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
+                    تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای
+                    کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و
+                    آینده
+                  </p>
+                </div>
+                {/* user single comment */}
+                <div className="flex flex-col border-b even:bg-[#F3F4F8] border-gray-400 p-4  my-2">
+                  <div className="flex flex-row justify-between">
+                    <a className="text-base font-bold ">ارشک یادگار</a>
+                    <a>1392/01/01</a>
+                  </div>
+
+                  <p className="text-sm text-justify mt-4">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
+                    تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای
+                    کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و
+                    آینده
+                  </p>
+                </div>
+                {/* user single comment */}
+                <div className="flex flex-col border-b even:bg-[#F3F4F8] border-gray-400 p-4  my-2">
+                  <div className="flex flex-row justify-between">
+                    <a className="text-base font-bold ">ارشک یادگار</a>
+                    <a>1392/01/01</a>
+                  </div>
+
+                  <p className="text-sm text-justify mt-4">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
+                    تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای
+                    کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و
+                    آینده
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
         <div></div>
-      </div>
-
-      {/* related products */}
-      <div className="p-4 mx-4 bg-white    shadow-md shadow-gray-300 my-4 ">
-        <div className="flex flex-row border-b-4 mb-4 px-2 py-1 border-[#EF5621] text-right font-bold">
-          <div className="">
-            <a>محصولات مرتبط</a>
-          </div>
-        </div>
       </div>
     </>
   );
