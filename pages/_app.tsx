b409027f6b51c734cb06304f12d84529c251aaf9
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-27 02:53:42 +0330

    i refrenced context in _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,35 @@
 import MainLayout from "@/components/common/mainLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import myAppContext from "@/components/context/context";
+import { useContext, useState } from "react";
 
+import { CityEntity, StateEntity } from "@/models/entities";
 export default function App({ Component, pageProps }: AppProps) {
+  const stateList: Array<StateEntity> = [];
+  const cityList: Array<CityEntity> = [];
+
+  const [states, setStates] = useState(stateList);
+  const [cities, setCities] = useState(cityList);
+
+  const [citiesUlToggle, setCitiesUlToggle] = useState(false);
+  const [statesUlToggle, setStatesUlToggle] = useState(false);
   return (
-    <MainLayout >
-      <Component {...pageProps} />
-    </MainLayout>
+    <myAppContext.Provider
+      value={{
+        states,
+        setStates,
+        cities,
+        setCities,
+        citiesUlToggle,
+        setCitiesUlToggle,
+        statesUlToggle,
+        setStatesUlToggle,
+      }}
+    >
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </myAppContext.Provider>
   );
 }
