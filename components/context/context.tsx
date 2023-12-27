d446457f78e9b1aa688c326ca624ceba1b004046
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-27 11:47:57 +0330

    i added factorform to context

--- components/context/context.tsx
+++ components/context/context.tsx
@@ -1,14 +1,26 @@
 import { createContext, useContext, useState } from "react";
-import { CityEntity, StateEntity } from "../../models/entities";
+import { CityEntity, FactorForm, StateEntity } from "../../models/entities";
 const myAppContext = createContext({
   cities: Array<CityEntity>(),
   setCities: (cities: any[]) => {},
   states: Array<StateEntity>(),
   setStates: (states: any[]) => {},
-  citiesUlToggle:false,
-  setCitiesUlToggle:(citiesUlToggle: boolean) => {},
-  statesUlToggle:false,
-  setStatesUlToggle:(statesUlToggle: boolean) => {},
+  citiesUlToggle: false,
+  setCitiesUlToggle: (citiesUlToggle: boolean) => {},
+  statesUlToggle: false,
+  setStatesUlToggle: (statesUlToggle: boolean) => {},
+  factorForm: {
+    formIsValid: false,
+    fNameError: "",
+    lNameError: "",
+    mobileError: "",
+    phoneError: "",
+    addressError: "",
+    stateError: "",
+    cityError: "",
+    postCodeError: "",
+  },
+  setFactorForm: (commentForm: FactorForm) => {},
 });
 
 export default myAppContext;
