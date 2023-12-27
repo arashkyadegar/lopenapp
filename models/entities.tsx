433915d5460b93dec797458225826efe811644d3
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-27 11:46:22 +0330

    i created factorFormEntity

--- models/entities.tsx
+++ models/entities.tsx
@@ -8,7 +8,31 @@ export class CityEntity {
   stateId: string = "";
 }
 
+export class FactorEntity {
+  id: string = "";
+  fName: string = "";
+  lName: string = "";
+  mobile: string = "";
+  phone: string = "";
+  address: string = "";
+  state: string = "";
+  city: string = "";
+  postCode: string = "";
+  info: string = "";
+}
+export class FactorForm extends FactorEntity {
+  formIsValid: boolean = false;
+  fNameError: string = "";
+  lNameError: string = "";
+  mobileError: string = "";
+  phoneError: string = "";
+  addressError: string = "";
+  stateError: string = "";
+  cityError: string = "";
+  postCodeError: string = "";
+}
 module.exports = {
   StateEntity,
   CityEntity,
+  FactorForm
 };
