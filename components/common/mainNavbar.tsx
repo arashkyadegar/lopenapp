e2dc64c0069b72c7f777b507ef34560eabb0c127
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-28 01:39:04 +0330

    i replaced smal basket compoennt

--- components/common/mainNavbar.tsx
+++ components/common/mainNavbar.tsx
@@ -2,8 +2,25 @@
 import React, { PropsWithChildren, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import BasketSmallComponent from "../basket_smal";
+import myAppContext from "@/components/context/context";
+import { useMediaQuery } from "react-responsive";
 export default function MainNavBar({ children }: PropsWithChildren) {
+  const isBigScreen = useMediaQuery({ query: "(min-width: 640px)" });
+
+  const { smallBasketToggle, setSmallBasketToggle } =
+    React.useContext(myAppContext);
+  const { navbarToggle, setNavbarToggle } = React.useContext(myAppContext);
+  function toggleSmallBasket() {
+    setSmallBasketToggle(!smallBasketToggle);
+  }
+
+  function toggleNavbar() {
+    setNavbarToggle(!navbarToggle);
+  }
+  if (isBigScreen) {
+    setNavbarToggle(true);
+  }
   return (
     <>
       <div className="flex border border-[#F3F4F8] bg-[#F3F4F8]  flex-row-reverse relative justify-between px-4 items-center ">
@@ -35,6 +52,9 @@ export default function MainNavBar({ children }: PropsWithChildren) {
               <a>12</a>
             </div>
             <svg
+              onClick={() => {
+                toggleSmallBasket();
+              }}
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -50,129 +70,7 @@ export default function MainNavBar({ children }: PropsWithChildren) {
               />
             </svg>
           </div>
-          <div className="absolute z-40 bg-white w-11/12  sm:w-5/12 left-5 top-10  shadow-md shadow-gray-500">
-            <div className=" grid w-full grid-cols-7 gap-2 font-bold text-xs border items-center justify-between border-gray-300 p-2">
-              {/* grids title row */}
-              <div className="col-span-1 text-center">
-                <a>#</a>
-              </div>
-              <div className="col-span-1 text-center">
-                <a>تصویر</a>
-              </div>
-              <div className="col-span-2 text-right">
-                <a>محصول</a>
-              </div>
-              <div className="col-span-1 text-center">
-                <a>تعداد</a>
-              </div>
-              <div className="col-span-1  text-left">
-                <a>تخفیف</a>
-              </div>
-              <div className="col-span-1  text-center">
-                <a>قیمت</a>
-              </div>
-            </div>
-            <div className=" grid w-full grid-cols-7 gap-2 text-xs border items-center justify-between border-gray-300 p-2">
-              {/* grids body */}
-              <div className="col-span-1 text-center ">
-                <a>1</a>
-              </div>
-              <div className="col-span-1 ">
-                <a>
-                  <Image
-                    src="/images1.jpg"
-                    width={500}
-                    height={500}
-                    className=" w-6 h-6 mx-auto"
-                    alt="user avator"
-                  />
-                </a>
-              </div>
-              <div className="col-span-2 ">
-                <a>ابنبات هلدار</a>
-              </div>
-              <div className="col-span-1 text-center">
-                <a>1</a>
-              </div>
-              <div className="col-span-1 text-left">
-                <a className=" text-red-400">1000</a>
-              </div>
-              <div className="col-span-1 text-center">
-                <a>2000</a>
-              </div>
-
-              <div className="col-span-1  text-center ">
-                <a>1</a>
-              </div>
-              <div className="col-span-1">
-                <a>
-                  <Image
-                    src="/images3.jpg"
-                    width={500}
-                    height={500}
-                    className=" w-6 h-6  mx-auto"
-                    alt="user avator"
-                  />
-                </a>
-              </div>
-              <div className="col-span-2">
-                <a>ابنبات هلدار</a>
-              </div>
-              <div className="col-span-1  text-center">
-                <a>2</a>
-              </div>
-              <div className="col-span-1 text-left">
-                <a className=" text-red-400 ">500</a>
-              </div>
-              <div className="col-span-1 text-center">
-                <a>4500</a>
-              </div>
-
-              <div className="col-span-1  text-center">
-                <a>1</a>
-              </div>
-              <div className="col-span-1">
-                <a>
-                  <Image
-                    src="/images2.jpg"
-                    width={500}
-                    height={500}
-                    className=" w-6 h-6  mx-auto"
-                    alt="user avator"
-                  />
-                </a>
-              </div>
-              <div className="col-span-2">
-                <a>ابنبات هلدار</a>
-              </div>
-              <div className="col-span-1  text-center">
-                <a>4</a>
-              </div>
-              <div className="col-span-1 text-left">
-                <a className=" text-red-400 ">3000</a>
-              </div>
-              <div className="col-span-1  text-center ">
-                <a>2500</a>
-              </div>
-
-              <div className="col-span-1   ">
-                <a href="" className="underline">
-                  سبد خرید
-                </a>
-              </div>
-              <div className="col-span-2  ">
-                <a href="" className="underline">
-                  تسویه حساب
-                </a>
-              </div>
-              <div className="col-span-1   col-start-6">
-                <a>قیمت کل</a>
-              </div>
-              <div className="col-span-1 text-center">
-                <a>10000</a>
-              </div>
-            </div>
-          </div>
+          {smallBasketToggle && <BasketSmallComponent />}
         </div>
         {/* language select */}
         <div className="text-xs">
@@ -197,6 +95,9 @@ export default function MainNavBar({ children }: PropsWithChildren) {
           {/* hambergur-button */}
           <div className="flex space-x-3 md:space-x-0">
             <button
+              onClick={() => {
+                toggleNavbar();
+              }}
               data-collapse-toggle="navbar-sticky"
               type="button"
               className="inline-flex transition-all duration-300 items-center ml-2 w-10 h-10 justify-center text-sm   rounded-lg md:hidden hover:text-[#80BB01] focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -238,60 +139,63 @@ export default function MainNavBar({ children }: PropsWithChildren) {
             </span>
           </a>
           {/* navbar-menu */}
-          <div
-            className="justify-start   order-3 md:order-none w-full md:flex md:w-auto md:mr-10 mr-0"
-            id="navbar-sticky"
-          >
-            <ul
-              className="text-base gap-2  flex flex-col text-right  text-gray-600  p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse 
-            md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          {navbarToggle && (
+            <div
+              className="justify-start   order-3 md:order-none w-full md:flex md:w-auto md:mr-10 mr-0"
+              id="navbar-sticky"
             >
-              <li className="border-[#FEEBB4] ml-4 hover:border-[#FEEBB4] transition-all duration-300 ">
-                <Link
-                  href={{
-                    pathname: `/`,
-                  }}
-                >
-                  صفحه اصلی
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={{
-                    pathname: `/checkout`,
-                  }}
-                >
-فاکتور                </Link>
-              </li>
-              <li>
-                <Link
-                  href={{
-                    pathname: `/about`,
-                  }}
-                >
-                  درباره ما
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={{
-                    pathname: `/contactus`,
-                  }}
-                >
-                  ارتباط با ما
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={{
-                    pathname: `/singleproduct`,
-                  }}
-                >
-                  محصول
-                </Link>
-              </li>
-            </ul>
-          </div>
+              <ul
+                className="text-base gap-2  flex flex-col text-right  text-gray-600  p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse 
+            md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+              >
+                <li className="border-[#FEEBB4] ml-4 hover:border-[#FEEBB4] transition-all duration-300 ">
+                  <Link
+                    href={{
+                      pathname: `/`,
+                    }}
+                  >
+                    صفحه اصلی
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={{
+                      pathname: `/checkout`,
+                    }}
+                  >
+                    فاکتور{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={{
+                      pathname: `/about`,
+                    }}
+                  >
+                    درباره ما
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={{
+                      pathname: `/contactus`,
+                    }}
+                  >
+                    ارتباط با ما
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={{
+                      pathname: `/singleproduct`,
+                    }}
+                  >
+                    محصول
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </>
