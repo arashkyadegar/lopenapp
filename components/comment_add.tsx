e5917d63213588b307fb514541054f389886ba4d
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-29 12:36:08 +0330

    i moved comment adding form from single product page to comment_add.tsx

---
+++ components/comment_add.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+
+import Image from "next/image";
+import React from "react";
+export default function CommentAddComponent({ props }: any) {
+  return (
+    <>
+      <div className="flex flex-row gap-2 mb-4 p-4">
+        <div>
+          <a>متن </a>
+        </div>
+        <textarea
+          className="w-9/12 border p-2 border-gray-300 outline-none"
+          name=""
+          id=""
+          cols={35}
+          rows={3}
+        ></textarea>
+        <button className="px-2 bg-slate-300 hover:bg-[#80BB01] w-2/12 h-fit">
+          ارسال
+        </button>
+      </div>
+    </>
+  );
+}
