db34e2b6edbac5b97b51ddd700e6c751e4127e2c
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-30 00:52:32 +0330

    i edited productcard

--- components/product_card.tsx
+++ components/product_card.tsx
@@ -2,24 +2,36 @@ import { PropsWithChildren } from "react";
 
 import Image from "next/image";
 import React from "react";
+import ScoreComponent from "./score";
 export default function ProductCardComponent({ props }: any) {
   const product = props;
+  console.log(product);
+  let discount = 0;
+  let newPrice = 0;
+  if (product.discounts.value != undefined) {
+    discount = product.discounts.value;
+    newPrice = product.price - (discount / 100) * product.price;
+  }
+
   return (
     <>
       {/* single product card */}
       <div className="text-right relative text-sm w-full max-w-sm bg-white    border border-[#FBF6EE] hover:border-[#FFB534] transition duration-200 shadow dark:bg-gray-800 dark:border-gray-700">
-        <span className="flex left-0 font-bold bg-red-600  items-center w-16 h-6 justify-center text-center text-xs absolute text-white">
-          <a>15%</a>
-        </span>
+        {discount > 0 && (
+          <span className="flex left-0 font-bold bg-red-600  items-center w-16 h-6 justify-center text-center text-xs absolute text-white">
+            <a>{discount} %</a>
+          </span>
+        )}
         <div className="flex mb-2 h-44  overflow-hidden mt-6 justify-center">
           <div>
             <a href="#" className=" w-full h-full">
               <Image
-                src="/images1.jpg"
+                src={product.image}
                 width={500}
                 height={500}
                 className=" hover:scale-110 w-fit aspect-auto transition duration-500 cursor-pointer"
-                alt="product image"
+                alt={product.name}
+                title={product.name}
               />
             </a>
           </div>
@@ -27,79 +39,37 @@ export default function ProductCardComponent({ props }: any) {
 
         <div className="px-5 pb-5 ">
           <div className="flex flex-row gap-4 mb-2  justify-center">
-            <a href="#">
-              <h1 className="text-left text-[#80BB01]  line-clamp-1  font-semibold tracking-tight dark:text-white">
-                15000 تومان
-              </h1>
-            </a>
+            {discount > 0 && (
+              <>
+                <a href="#">
+                  <h1 className="text-left text-[#80BB01]  line-clamp-1  font-semibold tracking-tight dark:text-white">
+                    {newPrice} تومان
+                  </h1>
+                </a>
+                <a href="#">
+                  <h1 className="line-through  line-clamp-1 font-semibold tracking-tight  dark:text-white">
+                    {product.price} تومان
+                  </h1>
+                </a>
+              </>
+            )}
 
-            <a href="#">
-              <h1 className="line-through  line-clamp-1 font-semibold tracking-tight  dark:text-white">
-                18000 تومان
-              </h1>
-            </a>
+            {discount === 0 && (
+              <a href="#">
+                <h1 className="text-left text-[#80BB01]  line-clamp-1  font-semibold tracking-tight dark:text-white">
+                  {product.price} تومان
+                </h1>
+              </a>
+            )}
           </div>
 
           <a href="#">
             <h5 className="text-base mb-4 text-center font-semibold tracking-tight dark:text-white">
-              آبنبات مخصوص هلدار
+              {product.name}
             </h5>
           </a>
           <div className="flex flex-col gap-4 sm:flex-row-reverse w-full justify-between  items-center  mt-2.5 ">
-            <div className="flex flex-row gap-2">
-              <div>
-                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-                  5.0
-                </span>
-              </div>
-              <div className="flex flex-row items-center  rtl:space-x-reverse">
-                <svg
-                  className="w-4 h-4 text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-4 h-4 text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-4 h-4 text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-4 h-4 text-yellow-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-4 h-4 text-gray-200 dark:text-gray-600"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-              </div>
-            </div>
+            <ScoreComponent props={product.score} />
             <div className=" bg-slate-300 px-2 py-1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -116,6 +86,7 @@ export default function ProductCardComponent({ props }: any) {
                   d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
                 />
               </svg>
+              
             </div>
           </div>
         </div>
