475b4a1e405667f1fae87ffcde0d37e74045a7a2
arashk yadegar <yadegar.arashk@gmail.com@>
2023-12-31 12:16:07 +0330

    i created a reducer for factor

---
+++ redux/store/factor.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { apiCallBegan } from "./api";
+//import { AuthToken } from "@/utility/readToken";
+
+//Action types
+
+const REMOVE_POST = "REMOVE_FACTOR";
+
+// Part 1
+export interface FactorInitialState {
+  factors: [];
+}
+export const initialState: FactorInitialState = {
+  factors: [],
+};
+
+// Part 2
+export const factorSlice = createSlice({
+  name: "factor",
+  initialState: {
+    list: [],
+    isLoading: false,
+    lastFetch: null,
+  },
+  reducers: {
+    factorsRecieved: (state: any, action: PayloadAction<any>) => {
+      state.list = action.payload;
+      state.lastFetch = Date.now();
+    },
+    factorAdded: (state: any, action: PayloadAction<number>) => {
+     state.list.push(action.payload);
+    },
+  },
+});
+
+
+
+export const { factorAdded } = factorSlice.actions;
+export default factorSlice.reducer;
